84dc0eb483bd8cffe04c98b557e59c0c7ed5f0ae
Philipp Zoechner <p.zoechner@gmail.com>
2016-02-29 11:54:32 +0100

    formal changes

--- PZEasyWebSpeech.js
+++ PZEasyWebSpeech.js
@@ -4,7 +4,8 @@
  * This JS-file makes it really easy to add speech support to your website.
  * As a workaround for Chrome Browser, the speech is stopped after every full
  * stop. To enhance the quality of speech, several (mostly closing) HTML-Tags
- * are converted to full stops - feel free to add you own by modifying "text".
+ * are converted to full stops - feel free to add your own by modifying the
+ * "text" variable.
  *
  * An example of what the library does can be found here:
  * @homepage http://fh.zoechner.com/branson/career/overview/
@@ -13,7 +14,6 @@
  * @author @homepage http://www.zoechner.com
  *
  * @version 1.0.1
- * @time 02/13/16
  *
  * Further information about speech synthesis can be found here:
  * https://github.com/AurelioDeRosa/HTML5-API-demos/blob/master/demos/speech-synthesis-api-demo.html
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # PZEasyWebSpeech
----
-This JS-file makes it really easy to add speech support to your website. As a workaround for Chrome Browser, the speech is stopped after every full stop. To enhance the quality of speech, several (mostly closing) HTML-Tags are converted to full stops - feel free to add you own by modifying "text".
+
+This JS-file makes it really easy to add speech support to your website. As a workaround for Chrome Browser, the speech is stopped after every full stop. To enhance the quality of speech, several (mostly closing) HTML-Tags are converted to full stops - feel free to add your own by modifying the "text" variable.
 
 ## Example
 An example of what the library does can be found [here](http://fh.zoechner.com/branson/career/overview/).
